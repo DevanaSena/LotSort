749eae7400f5b6b55612e629ec29e8ecc75d6608
Devanasena <watssenna@gmail.com>
2024-05-31 21:46:06 -0300

    Iniciando Projeto

---
+++ projetoloteria/src/test/java/br/com/whiletrue/projetoloteria/TestProjetoloteriaApplication.java
@@ -0,0 +1,23 @@
+package br.com.whiletrue.projetoloteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestProjetoloteriaApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(ProjetoloteriaApplication::main).with(TestProjetoloteriaApplication.class).run(args);
+	}
+
+}
